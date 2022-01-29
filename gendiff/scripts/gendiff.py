cac9c2630b72f80ee5a401825979e8d6736ccf8b
Mikhail Gurin <mikhail.gurin@gmail.com>
2022-01-29 20:42:53 +0300

    step 1

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+args = parser.parse_args()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
